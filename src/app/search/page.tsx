9e4d59590e67d56b49cfc05982e6d53665fac4b7
AnnRozhkina <annrozhkina@gmail.com>
2024-04-07 23:39:49 +0300

    add suspence for static page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,40 +1,14 @@
-'use client'
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState, Suspense } from 'react';
-import { productsArray } from '../../../data.js';
-import Product from '../types.js';
-import ProductCard from '@/components/products/components/ProductCard';
+import SearchContent from '@/components/search/SearchContent';
+import React, { Suspense } from 'react';
+
 
 export default function SearchPage() {
-  const searchParams = useSearchParams()
-  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
-
-  const searchValue = searchParams.get('q');
-  const lowercaseSearchValue = searchValue ? searchValue.toLowerCase() : '';
-
-  useEffect(() => {
-    if (searchParams) {
-      const filteredProducts = productsArray.filter(product =>
-        product.title.toLowerCase().includes(lowercaseSearchValue)
-      );
-      setFilteredProducts(filteredProducts);
-    }
-  }, [searchParams, lowercaseSearchValue]);
 
   return (
-    <>
+    // <>
       <Suspense fallback={<div>Loading...</div>}>
-      <div className=" max-w-[350px] sm:max-w-[650px] md:max-w-[924px] lg:max-w-[1024px] px-[20px] sm:p-[10px] my-0 mx-auto ">
-      <h1 className='text-xl'>{`Результати пошуку за запитом ${searchValue}`}</h1>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  pt-7 pb-[75px]' >
-        {filteredProducts.map(product =>
-        (<div className='p-0 relative' key={product.id}>
-            < ProductCard  product={product} />
-          </div>)
-          )}
-        </div>
-        </div>
+        <SearchContent />
       </Suspense>
-    </>
+    // </>
   );
 }
